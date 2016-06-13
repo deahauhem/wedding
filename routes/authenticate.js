4843da66bae3e225d957d34220394728972d2ac3
deahauhem <wayne.yeap@gmail.com>
2016-06-13 12:14:30 +1000

    Add facebook authentication

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,42 +1,66 @@
 var express = require('express'),
     url = require('url'),
     querystring = require('querystring');
+var authentication = require('../authentication.json')
 
 var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+    LocalStrategy = require('passport-local').Strategy,
+    FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.use(new LocalStrategy({session: true}, (username, password, done) => {
   if (username === 'foo' && password === 'bar') {
-    done(null, { id: username });
+    done(null, { id: `local:${username}` });
   } else {
     done(null, false);
   }
 }));
-// TODO: read the docs
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-// TODO: read the docs
+passport.use(new FacebookStrategy({
+  clientID: authentication.facebook.appid,
+  clientSecret: authentication.facebook.secret,
+  callbackURL: 'http://localhost:3000/authenticate/fbcallback'
+}, (accessToken, refreshToken, profile, done) => {
+  done(null, {id: `facebook:${profile.id}`, name: profile.displayName});
+}));
+
+passport.serializeUser((user, done) => { done(null, user); });
 passport.deserializeUser((user, done) => { done(null, user); });
 
 var ensureAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) { return next(); }
+  if (req.isAuthenticated()) { 
+    return next(); 
+  }
+  console.log(`not authenticated. session: ${JSON.stringify(req.session)}`);
   req.error = 'Please sign in!';
+  req.session.redirectTo = req.originalUrl;
   res.redirect('/authenticate/login');
 };
 
 var router = express.Router();
 router.get('/login',
-    (req, res, next) => { res.render('authentication/login'); }
+    (req, res, next) => { 
+      console.log(`GET headers: ${JSON.stringify(req.headers)}`);
+      res.render('authentication/login'); 
+    }
 );
-router.get('/profile', ensureAuthenticated,
+router.post('/login', passport.authenticate('local'),
+    (req, res, next) => { 
+      console.log(`POST session: ${JSON.stringify(req.session)}`);
+      var redirectTo = req.session.redirectTo;
+      req.session.redirectTo = null;
+      res.redirect(redirectTo); 
+    }
+);
+
+router.get('/fblogin', passport.authenticate('facebook'));
+router.get('/fbcallback', passport.authenticate('facebook', {successRedirect: '/', failureRedirect: '/login'}));
+
+router.use('/', ensureAuthenticated);
+
+router.get('/profile',
     (req, res, next) => { res.render('authentication/profile', {user: req.user} ); }
 );
 
-router.get('/', ensureAuthenticated,
-    (req, res, next) => { res.render('authentication/index'); }
-);
-router.post('/login', passport.authenticate('local'),
+router.get('/',
     (req, res, next) => { res.render('authentication/index'); }
 );
 
